574bbc8efa386fec0bb1a4287acefcd9f3ca8173
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2024-08-21 11:37:32 +0530

    added signup checks (if email already exists)

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -14,10 +14,12 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
 export const signUpUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { name, email, password } = req.body;
+        if (users.findOne({ email: email }))
+            return res.status(422).json({ error: "Email already exists" });
         const hashedPassword = (await hash(password, 10)).toString();
         const user = new users({ name, email, password: hashedPassword });
         await user.save();
-        return res.status(200).json({ message: "OK", id: user._id.toString() });
+        return res.status(200).json({ message: "User registered successfully", id: user._id.toString() });
     } catch (error) {
         return res.json({ message: "ERROR", cause: error.message });
     }
