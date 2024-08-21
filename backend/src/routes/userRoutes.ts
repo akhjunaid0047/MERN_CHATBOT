7ed29f81091b284469614fc1590a6c6e0f9f034f
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2024-08-21 11:24:46 +0530

    added validations

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllUsers, signUpUser } from "../controllers/userController.js";
+import { signUpValidator, validate } from "../utils/validators.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/",getAllUsers);
-userRoutes.post("/signup",signUpUser);
+userRoutes.post("/signup",validate(signUpValidator),signUpUser);
 
 export default userRoutes;
