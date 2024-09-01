9b4137ace556c8f6d6e66905313e49649e16c1a0
Khandakar Aryanul Haque <aryanul2004@gmail.com>
2024-09-01 18:43:52 +0530

    frontend update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme, ThemeProvider } from "@mui/material";
+import {BrowserRouter} from "react-router-dom";
+import {AuthProvider} from "./context/AuthContext.tsx";
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:3000/api/v1/user/login';
+
+const theme = createTheme({
+   typography: {fontFamily:"Robot Slab,serif", allVariants: {color: "white"}},
+  });
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter> 
+        <ThemeProvider theme={theme}> 
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
-)
+);
