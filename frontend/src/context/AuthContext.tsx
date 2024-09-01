ad270096f562bef8421892204da17795e2e401b0
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2024-09-01 21:16:34 +0530

    added signup page and functionality

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from "react";
-import { checkAuthStatus, loginUser, logoutUser } from "../helper/apiCommunicator";
+import { checkAuthStatus, loginUser, logoutUser, signupUser } from "../helper/apiCommunicator";
 
 
 type User = {
@@ -41,8 +41,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
             setIsLoggedIn(true);
         }
     };
-    const signup = async (name: string, email: string, password: string) => { };
-    const logout = async () => { 
+    const signup = async (name: string, email: string, password: string) => {
+        const data = await signupUser(name, email, password);
+        if (data) {
+            setUser({ email: data.email, name: data.name });
+            setIsLoggedIn(true);
+        }
+    };
+    const logout = async () => {
         await logoutUser();
         setIsLoggedIn(false);
         setUser(null);
--- frontend/src/helper/apiCommunicator.ts
+++ frontend/src/helper/apiCommunicator.ts
@@ -24,3 +24,16 @@ export const logoutUser = async () => {
     const data = await res.data;
     return data;
 };
+
+export const signupUser = async (
+    name: string,
+    email: string,
+    password: string
+  ) => {
+    const res = await axios.post("/user/signup", { name, email, password });
+    if (res.status !== 201) {
+      throw new Error("Unable to Signup");
+    }
+    const data = await res.data;
+    return data;
+  };
